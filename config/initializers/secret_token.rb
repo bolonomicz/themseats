15bd4bbc8387639e06a80c1699ffba3d3e09a512
Ralph Bolo <ralph.bolo@gmail.com>
2013-09-22 11:01:37 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Themseats::Application.config.secret_token = '7bc11a69459a8f4ce60408b89dec8dfd2a6b8b6915c15b8bbf4735daf5f035b53b722296edda9573c8817048d271e56a57519297438aa1c3c0b6d13e0732571e'
