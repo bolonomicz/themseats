3de5b2f566c499fd2c74a070cd3a97c8712889b7
Ralph Bolo <ralph.bolo@gmail.com>
2013-09-22 11:29:17 -0400

    create restaurants controller and actions

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,43 @@
+class RestaurantsController < ApplicationController
+
+	def index
+		@restaurants = Restaurant.all
+	end
+
+	def show
+		@restaurant = Restaurant.find(params[:id])
+	end
+
+	def new
+		@restaurant = Restaurant.new
+	end
+	def create
+		@restaurant = Restaurant.new(params[:restaurant])
+		@restaurant.save
+
+		flash.notice = "Restaurant #{@restaurant.name} was registered"
+		redirect_to restaurant_path(@restaurant)
+		
+	end
+
+	def edit
+		@restaurant = Restaurant.find(params[:id])
+		
+	end
+
+	def update
+		@restaurant = Restaurant.find(params[:id])
+		@restaurant.update_attributes(params[:restaurant])
+
+		flash.notice = "Restaurant #{@restaurant.name} was updated"
+
+		redirect_to restaurant_path(@restaurant)
+		
+	end
+
+	def destroy
+		@restaurant.find(params[:id])
+		@restaurant.destroy, :flash => "Restaurant has been delisted"
+		
+	end
+end
